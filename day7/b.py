d92f30366d61eb77c3b47a8e796eefedf125bc0d
Nicolas CASAUX <nicolas.casaux@soprasteria.com>
2024-12-09 18:55:27 +0100

    Added day7

---
+++ day7/b.py
@@ -0,0 +1,28 @@
+import aocd
+
+
+def combine(expected_total, so_far_total, remaining_numbers):
+    m = False
+    if len(remaining_numbers) == 0:
+        if expected_total == so_far_total:
+            return True
+    else:
+        if combine(expected_total, so_far_total + remaining_numbers[0], remaining_numbers[1:]):
+            return True
+        if combine(expected_total, so_far_total * remaining_numbers[0], remaining_numbers[1:]):
+            return True
+        if combine(expected_total, int(str(so_far_total) + str(remaining_numbers[0])), remaining_numbers[1:]):
+            return True
+
+
+f = aocd.get_data(day=7, year=2024)
+line = f.split("\n\n")[0].splitlines()
+total = 0
+
+for l in line:
+    expectedTotal = int(l.split(":")[0])
+    soFarTotal = int(l.split(":")[1].split()[0])
+    remainingNumbers = [int(i) for i in l.split(":")[1].split()[1:]]
+    if combine(expectedTotal, soFarTotal, remainingNumbers): total += expectedTotal
+
+print(f"Total is: {total}")
